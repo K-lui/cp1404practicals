a22e6144117ff688633deaf0522788150d79f7fe
kevin <lui.kevin@my.jcu.edu.au>
2024-09-29 17:59:18 +1000

    Add prac 1 file

---
+++ prac_01/test.py
@@ -0,0 +1,57 @@
+# x = 10
+# while x > 4:
+#     print(x, end=" ")
+#     x = x - 2
+
+
+# total_age = 0
+#
+# number_of_age = int(input('How many ages to enter: '))
+# for i in range(number_of_age):
+#     ages = int(input('Enter ages: '))
+#     total_age += ages
+# average = total_age / number_of_age
+# print(f"Average: {average:.1f}\nTotal: {total_age}")
+
+
+# total_age = 0
+# average = 0
+# number_of_age = 0
+# 
+# age = int(input('Enter age: '))
+# while age >= 0:
+#     number_of_age += 1
+#     total_age = total_age + age
+#     age = int(input('Enter age: '))
+# average = total_age / number_of_age
+# print(f"Average: {average:.1f}\nTotal: {total_age}")
+
+#
+# for i in range(2):
+#     for j in range(2):
+#         print(i, "-", j)
+#
+# """which kind of loop?"""
+# 1.
+# for i in range(16, 100, 2):
+#     print(i)
+
+# 2.
+# name = input("name:")
+# while name != "":
+#     name = input("name:")
+
+# # 3.
+# text_file = "Living on a island, eating coconut and chilling under the trees."
+# for i in text_file:
+#     print(i)
+
+# 4.
+# number = 0
+#
+# sentence = "Living on a island, eating coconut and chilling under the trees."
+# for i in sentence:
+#     if i == "a" or "e" or "i" or "o" or "u":
+#         number += 1
+# print(number)
+
