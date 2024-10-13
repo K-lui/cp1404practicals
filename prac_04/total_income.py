3c68f6a9f826c16237d3f76fcb00ee4b21d9ea57
kevin <lui.kevin@my.jcu.edu.au>
2024-10-13 18:32:23 +1000

    Finished total income program

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -8,11 +8,9 @@ def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
     number_of_months = int(input("How many months? "))
-
     for number_of_month in range(1, number_of_months + 1):
-        income = float(input(f"Enter income for month " + str(number_of_month) + ": "))
+        income = float(input(f"Enter income for month {str(number_of_month)}: "))
         incomes.append(income)
-
     print_report(incomes, number_of_months)
 
 
@@ -22,7 +20,7 @@ def print_report(incomes, number_of_months):
     for number_of_month in range(1, number_of_months + 1):
         income = incomes[number_of_month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(number_of_month, income, total))
+        print("Month {:2} - Income: ${:10.2f}         Total: ${:10.2f}".format(number_of_month, income, total))
 
 
 main()
