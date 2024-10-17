7850cd451bebc0cb4af367eab37ef35998e4346d
kevin <lui.kevin@my.jcu.edu.au>
2024-10-17 14:35:44 +1000

    Choose 10 colours in hex colours program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,18 @@
+"""
+CP1404/CP5632 Practical
+Hex colours Program
+"""
 
+COLOUR_TO_CODE = {"AliceBlue": "#f0f8ff", "Amber": "#ffbf00", "Amethyst": "#9966cc", "Aqua": "#00ffff", "Beige":
+    "#f5f5dc", "Black": "#000000", "Blond": "#faf0be", "Bone": "#e3dac9", "Corn": "#fbec5d", "Cream": "#fffdd0"}
+print(COLOUR_TO_CODE)
 
-
+state_code = input("Enter short state: ").lower()
+while state_code != "":
+    try:
+        print(state_code, "is", COLOUR_TO_CODE[state_code])
+    except KeyError:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").lower()
+# for state_code in COLOUR_TO_CODE:
+#     print(f"{state_code:4} is {COLOUR_TO_CODE[state_code]}")
