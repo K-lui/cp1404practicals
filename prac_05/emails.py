9e3d3940f1f199e37614107d19240584dde6488f
kevin <lui.kevin@my.jcu.edu.au>
2024-10-18 23:19:30 +1000

    Complete prac 5 emails program with modifications

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,31 +6,30 @@ Emails Program
 """
 Emails
 Estimate: 60 minutes
-Actual:  227 minutes
+Actual:  230 minutes
 """
-
-EMAIL_TO_NAMES = {}
 
 
 def main():
+    """Create a dictionary of emails and names"""
+    email_to_name = {}
     user_email = input('Email: ')
     while user_email != "":
         name_of_user = extract_name_from_email(user_email)
-
-        # for name_of_user in strings_in_email:
         answer = input(f"Is your name {name_of_user.title()}? (Y/n) ").lower()
         if answer == "y" or answer == "yes":
-            EMAIL_TO_NAMES[user_email] = name_of_user
+            email_to_name[user_email] = name_of_user
         else:
             name_of_user = input("Name: ")
-            EMAIL_TO_NAMES[user_email] = name_of_user
+            email_to_name[user_email] = name_of_user
         user_email = input('Email: ')
 
-    for user_email, name_of_user in EMAIL_TO_NAMES.items():
+    for user_email, name_of_user in email_to_name.items():
         print(f"{name_of_user.title()} ({user_email})")
 
 
 def extract_name_from_email(user_email):
+    """Extract name from given email"""
     break_up_email = user_email.split('@')
     strings_in_email = [word.replace(".", " ") for word in break_up_email]
     return strings_in_email[0]
