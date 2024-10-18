8c302825d86fab9e070e091a26c0733548235e05
kevin <lui.kevin@my.jcu.edu.au>
2024-10-18 22:20:09 +1000

    Finish emails program without while loop

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -9,4 +9,27 @@ Estimate: 60 minutes
 Actual:      minutes
 """
 
+EMAIL_TO_NAMES = {}
+
+
+# user_email = input('Email: ')
+user_email = "kevin.lui@my.jcu.edu.au"
+
+break_up_email = user_email.split('@')
+strings_in_email = [word.replace(".", " ") for word in break_up_email]
+del strings_in_email[1]
+
+for name_of_user in strings_in_email:
+    answer = input(f"Is your name {name_of_user.title()}? (Y/n) ").lower()
+    if answer == "y" or answer == "yes":
+        EMAIL_TO_NAMES[user_email] = name_of_user
+    else:
+        name_of_user = input("Name: ")
+        EMAIL_TO_NAMES[user_email] = name_of_user
+user_email = input('Email: ')
+
+for user_email, name_of_user in EMAIL_TO_NAMES.items():
+    print(f"{name_of_user.title()} ({user_email})")
+
+
 
