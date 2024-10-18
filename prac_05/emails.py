c938190d36fd503fbb3da9aa18f4212b7255a36f
kevin <lui.kevin@my.jcu.edu.au>
2024-10-18 22:55:06 +1000

    Complete prac 5 emails program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,28 +6,34 @@ Emails Program
 """
 Emails
 Estimate: 60 minutes
-Actual:  204 minutes
+Actual:  227 minutes
 """
 
 EMAIL_TO_NAMES = {}
 
 
-user_email = input('Email: ')
-# user_email = "kevin.lui@my.jcu.edu.au"
-while user_email != "":
-    break_up_email = user_email.split('@')
-    strings_in_email = [word.replace(".", " ") for word in break_up_email]
-    del strings_in_email[1]
+def main():
+    user_email = input('Email: ')
+    while user_email != "":
+        name_of_user = extract_name_from_email(user_email)
 
-    for name_of_user in strings_in_email:
+        # for name_of_user in strings_in_email:
         answer = input(f"Is your name {name_of_user.title()}? (Y/n) ").lower()
         if answer == "y" or answer == "yes":
             EMAIL_TO_NAMES[user_email] = name_of_user
         else:
             name_of_user = input("Name: ")
             EMAIL_TO_NAMES[user_email] = name_of_user
-    user_email = input('Email: ')
+        user_email = input('Email: ')
 
-for user_email, name_of_user in EMAIL_TO_NAMES.items():
-    print(f"{name_of_user.title()} ({user_email})")
+    for user_email, name_of_user in EMAIL_TO_NAMES.items():
+        print(f"{name_of_user.title()} ({user_email})")
 
+
+def extract_name_from_email(user_email):
+    break_up_email = user_email.split('@')
+    strings_in_email = [word.replace(".", " ") for word in break_up_email]
+    return strings_in_email[0]
+
+
+main()
