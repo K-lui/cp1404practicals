40a15b3c4911630c7db4825f96d3c1bf76b353e5
kevin <lui.kevin@my.jcu.edu.au>
2024-10-18 18:07:24 +1000

    Complete word occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,10 +6,25 @@ Word occurrences Program
 """
 Word Occurrences
 Estimate: 60 minutes
-Actual:    minutes
+Actual:   64 minutes
 """
 
+text = "this is a collection of words of nice words this is a fun thing it is"
+words = (text.split())
+TEXT_TO_COUNT = {}
+print("Text:", text)
 
+string = input(">>> ")
+for word in words:
+    if word in TEXT_TO_COUNT:
+        TEXT_TO_COUNT[word] += 1
+    else:
+        TEXT_TO_COUNT[word] = 1
+
+width = max(len(word) for word in TEXT_TO_COUNT)
+
+for word, count in sorted(TEXT_TO_COUNT.items()):
+    print(f"{word:{width}} : {count}")
 
 
 
