cbdfffe19616b30bf480eb925402db653596666f
kevin <lui.kevin@my.jcu.edu.au>
2024-11-03 04:46:29 +1000

    Add guitars program

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -3,5 +3,20 @@ CP1404 Practical 06 - Guitars (The client program)
 
 Guitars
 Estimate: 60 minutes
-Actual:     minutes
+Actual:   minutes
 """
+
+from prac_06.guitar import Guitar
+
+guitars = []
+
+# guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+# guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+# guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
+
+
+
+
+
+
+
