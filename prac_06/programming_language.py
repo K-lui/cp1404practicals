85c6488a334a2bdd779ae76b80ccda8decd02aa1
kevin <lui.kevin@my.jcu.edu.au>
2024-11-02 20:13:08 +1000

    Complete prac 06 programming_language and languages program

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,12 +1,20 @@
-"""CP1404 Practical 06 - Programming Language"""
-
 """
+CP1404 Practical 06 - Programming Language
+
+
 Programming Language
 Estimate: 120 minutes
-Actual:       minutes
+Actual:    32 minutes
 """
 
+from prac_06.languages import ProgrammingLanguage
 
-
-
-
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+# print(python) # Ignore this line
+languages = [python, ruby, visual_basic]
+print("The dynamically typed languages are:")
+for row in languages:
+    if row.is_dynamic():
+        print(row.name)
