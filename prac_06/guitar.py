3259190ae3cf5afbe9e10697ff907e830e7aee48
kevin <lui.kevin@my.jcu.edu.au>
2024-11-03 01:40:56 +1000

    Finish testing guitar and guitar test program

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,3 +1,19 @@
 class Guitar:
 
     def __init__(self, name="", year=0, cost=0.0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self, current_year):
+        self.year = current_year - self.year
+        return f"{self.name} - Got {self.year}"
+
+    def is_vintage(self):
+        if self.year < 50:
+            return f"{self.name} - Got {False}"
+        else:
+            return f"{self.name} - Got {True}"
--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -5,7 +5,17 @@ Programming Language
 Estimate: 120 minutes
 Actual:       minutes
 """
+
 from prac_06.guitar import Guitar
+
+guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+guitar2 = Guitar("Another Guitar", 2013)
+guitars = [guitar1, guitar2]
+# print(f"My guitar: {name}, first made in {year}")
+# print(f"{name} ({year}) : ${cost:,.2f}")
+for guitar in guitars:
+    print(guitar.get_age(current_year=2022))
+    print(guitar.is_vintage())
 
 
 
