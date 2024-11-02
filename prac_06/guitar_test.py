3fc5084df5d2cde709fd4aadb74117e7d0fef9bc
kevin <lui.kevin@my.jcu.edu.au>
2024-11-03 01:53:41 +1000

    Complete prac 06 guitar_test

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,21 +1,21 @@
 """
 CP1404 Practical 06 - Guitar Test
 
-Programming Language
+Guitar Test
 Estimate: 120 minutes
-Actual:       minutes
+Actual:    38 minutes
 """
 
 from prac_06.guitar import Guitar
 
 guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
 guitar2 = Guitar("Another Guitar", 2013)
-guitars = [guitar1, guitar2]
-# print(f"My guitar: {name}, first made in {year}")
-# print(f"{name} ({year}) : ${cost:,.2f}")
-for guitar in guitars:
-    print(guitar.get_age(current_year=2022))
-    print(guitar.is_vintage())
+# guitars = [guitar1, guitar2]
+# for guitar in guitars:
+print(f"{guitar1.name} get_age() - Expected 100.", guitar1.get_age(current_year=2022))
+print(f"{guitar2.name} get_age() - Expected 9.", guitar2.get_age(current_year=2022))
+print(f"{guitar1.name} is_vintage() - Expected True.", guitar1.is_vintage())
+print(f"{guitar2.name} is_vintage() - Expected False.", guitar2.is_vintage())
 
 
 
