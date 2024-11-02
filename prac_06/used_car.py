13735c17edf9ec4218dd13f3356e6d536e864fb5
kevin <lui.kevin@my.jcu.edu.au>
2024-11-02 18:37:48 +1000

    Complete prac 06 used_car and car program

--- prac_06/used_car.py
+++ prac_06/used_car.py
@@ -9,10 +9,17 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(180, "My car")
     my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
+    print(f"My car has fuel: {my_car.fuel}")
+
+    limo = Car(100, "limo")
+    limo.add_fuel(20)
+    print(f"Limo has fuel: {limo.fuel}")
+    limo.drive(115)
+
     print(my_car)
+    print(limo)
 
 
 main()
