f1dfa43e7ba73a18186ab05cc3974dd8f3ade391
kevin <lui.kevin@my.jcu.edu.au>
2024-11-18 21:45:39 +1000

    Complete stage 1 of convert_miles_km.kv/py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,3 +3,55 @@ CP1404 Week 8 Workshop - GUI program to convert miles to kilometres
 Kevin Lui, IT@JCU
 18/11/2024
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
+
+
+class MilesConverterApp(App):
+    message = StringProperty()
+
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        self.message = "Type in the text entry field"
+        return self.root
+
+    def handle_calculate(self):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        value = self.get_validated_miles()
+        result = value * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
+
+    def handle_increment(self, change):
+        """
+        handle up/down button press, update the text input with new value, call calculation function
+        :param change: the amount to change
+        """
+        value = self.get_validated_miles() + change
+        self.root.ids.input_miles.text = str(value)
+        # self.handle_calculate()
+        self.handle_update()
+
+    def get_validated_miles(self):
+        """
+        get text input from text entry widget, convert to float
+        :return: 0 if error, float version of text if valid
+        """
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+    def handle_update(self):
+        """Handle changes to the text input by updating the model from the view."""
+        self.message = self.root.ids.input_miles.text
+        self.handle_calculate()
+
+
+MilesConverterApp().run()
