7b3fd6e4e5b447f4a43c2ebe0671d523da147dc3
kevin <lui.kevin@my.jcu.edu.au>
2024-11-24 03:52:22 +1000

    Complete silver_service_taxi_test.py and silver_service_taxi.py

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,3 +1,17 @@
 """
 CP1404 - Silver Service Taxi test
 """
+
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    taxi = SilverServiceTaxi("Silver Service Taxi", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(f"${taxi.get_fare():.2f}")
+
+
+main()
+
+
